82db641b66087ccaee12e6173c0fe6d00f631f84
JonPaulCode <jwatson88@live.co.uk>
2022-02-11 18:42:00 +0000

    Added final game function, checks players choice vs computers choice and checks who won.

--- scripts/gamescript.js
+++ scripts/gamescript.js
@@ -3,7 +3,7 @@ function computerPlay() {
   return choices[Math.floor(Math.random() * 3)];
 }
 
-function playRound() {
+function playRound(playerChoice, computerChoice) {
   if (playerChoice === 'rock' && computerChoice === 'scissors') {
     return 'player';
   } else if (playerChoice === 'paper' && computerChoice === 'rock') {
@@ -14,4 +14,25 @@ function playRound() {
   return 'computer';
 }
 
-function game() {}
+function game() {
+  let playerChoice;
+  let computerChoice;
+  let winner;
+  let playerWins = 0;
+  let computerWins = 0;
+  for (let i = 0; i < 2; i++) {
+    computerChoice = computerPlay();
+    console.log(computerChoice);
+    playerChoice = prompt('Input Choice:').toLowerCase();
+    winner = playRound(playerChoice, computerChoice);
+    if (winner === 'player') {
+      playerWins += 1;
+    } else {
+      computerWins += 1;
+    }
+    alert(`${winner} Wins!
+Computer Score: ${computerWins} --- Player Score: ${playerWins}`);
+  }
+}
+
+game();
