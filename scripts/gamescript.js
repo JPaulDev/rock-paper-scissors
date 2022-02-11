e43af3a9527d7e6e5f72cbac0d40d8aa7cf423f9
JonPaulCode <jwatson88@live.co.uk>
2022-02-11 11:33:39 +0000

    Added the playRound function which handles win/loss conditions and returns the winner.

--- scripts/gamescript.js
+++ scripts/gamescript.js
@@ -3,6 +3,15 @@ function computerPlay() {
   return choices[Math.floor(Math.random() * 3)];
 }
 
-function playRound() {}
+function playRound() {
+  if (playerChoice === 'rock' && computerChoice === 'scissors') {
+    return 'player';
+  } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+    return 'player';
+  } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+    return 'player';
+  }
+  return 'computer';
+}
 
 function game() {}
